ecf09a7f4c0fe8ea7279e0fcfe53a20de5c14c1a
RostykGnat <149826109+RostykGnat@users.noreply.github.com>
2024-11-19 20:05:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Створіть функцію-конструктор Calculator, який створює об’єкти з трьома
+// методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у
+// властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей
+
+// function Calculator() {
+//   this.read = function () {
+//     this.a = +prompt("перше число:", 0);
+//     this.b = +prompt("друге число:", 0);
+//   };
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+//   this.mul = function () {
+//     return this.a * this.b;
+//   };
+// }
+// const calculator = new Calculator();
+// calculator.read();
+// alert("Сума: " + calculator.sum());
+// alert("Добуток: " + calculator.mul());
